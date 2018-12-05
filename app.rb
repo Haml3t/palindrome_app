2670421f1177a9ca560493304ab2c2c822ed8b2a
David Sugarman <d.m.sugarman@gmail.com>
2018-12-05 15:53:07 -0400

    refactored

--- app.rb
+++ app.rb
@@ -1,13 +1,17 @@
 require 'sinatra'
 
 get '/' do
+  @title = 'Home'
+  @time_now = Time.now.asctime
   erb :index
 end
 
 get '/about' do
+  @title = 'About'
   erb :about
 end
 
 get '/palindrome' do
+  @title = 'Palindrome Detector'
   erb :palindrome
 end
